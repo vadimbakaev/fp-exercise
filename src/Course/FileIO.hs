1d32aab39d882dc9248fac25e2ed00f10016cf0f
Vadim Bakaev <bakaev.vad@gmail.com>
2019-04-28 22:10:17 +0200

    WIP on FileIO

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -85,47 +85,49 @@ printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
-
+printFile name file = do
+  _ <- putStrLn ("============ " ++ name)
+  putStrLn ("the montents of " ++  file)
+  
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles files = void $ sequence $ (\(f, b) -> printFile f b) <$> files 
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile fileName = (\c -> (fileName, c)) <$> readFile fileName
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles files = sequence $ getFile <$> files 
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@ and @printFiles@.
 run ::
   FilePath
-  -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+  -> IO ()  
+run fileName = do
+  file <- readFile fileName
+  files <- getFiles $ lines file
+  printFiles files
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
-main =
-  error "todo: Course.FileIO#main"
-
+main = do
+  fileName <- getArgs
+  _ <- sequence $ run <$> fileName
+  pure ()
+  
 ----
 
 -- Was there was some repetition in our solution?
